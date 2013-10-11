64ccbfed7103f9c057e600815fb312b4f1916608
Jim Baker <jim.baker@rackspace.com>
2013-10-11 16:11:15 -0600

    Remove unnecessary attribute writing

--- setup.py
+++ setup.py
@@ -6,15 +6,17 @@ from setuptools import setup, find_packages
 
 setup(
     name = "clamp",
-    version = "0.1",
+    version = "0.2",
     packages = find_packages(),
     entry_points = {
         "distutils.commands": [
             "buildjar = clamp.build:buildjar",
+            # add singlejar command
         ],
         "distutils.setup_keywords": [
             "clamp = clamp.build:validate_clamp",
         ],
+        # add singlejar script
     }
 )
 
